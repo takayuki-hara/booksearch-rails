e609dc842b8c36fdb00f63f3e3b66caeffd19eec
takayuki-hara <takayuki.hara.dev@gmail.com>
2018-11-18 22:16:32 +0900

    keyをNotNullに変更

--- db/migrate/20181118093123_create_apikeys.rb
+++ db/migrate/20181118093123_create_apikeys.rb
@@ -1,7 +1,7 @@
 class CreateApikeys < ActiveRecord::Migration[5.2]
   def change
     create_table :apikeys do |t|
-      t.string :key
+      t.string :key, :null => false
 
       t.timestamps
     end
