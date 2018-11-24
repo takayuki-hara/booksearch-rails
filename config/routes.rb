ed3d25eb263eb27f42bff591512969793af967bd
takayuki-hara <takayuki.hara.dev@gmail.com>
2018-11-24 17:28:39 +0900

    微調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'mypage/index'
   resources :users
   resources :apikeys
 
   get 'search/index'
   post 'search/search'
+
+  get 'mypage/index'
 
   get 'manage_console/index'
 
