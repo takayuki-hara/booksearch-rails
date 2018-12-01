20fd5adfae5ae8d3a11a3dd8f631c6b6bf061cf1
takayuki-hara <takayuki.hara.dev@gmail.com>
2018-12-01 13:41:48 +0900

    見た目の調整

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_24_070514) do
+ActiveRecord::Schema.define(version: 2018_12_01_033830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,14 @@ ActiveRecord::Schema.define(version: 2018_11_24_070514) do
     t.index ["title"], name: "index_books_on_title"
   end
 
+  create_table "keywords", force: :cascade do |t|
+    t.bigint "user_id"
+    t.integer "item_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_keywords_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
     t.string "password_digest", null: false
@@ -48,4 +56,5 @@ ActiveRecord::Schema.define(version: 2018_11_24_070514) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "keywords", "users"
 end
