daeba974de88c2b4418dd846a04a994c6ecf581f
takayuki-hara <takayuki.hara.dev@gmail.com>
2018-12-22 13:27:25 +0900

    検索処理を切り出し

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -16,7 +16,10 @@ class SearchController < ApplicationController
     else
       @page = 1
     end
-    fetch(@keywd, @page)
+    searcher = BookSearcher.new
+    @result = searcher.search(@keywd, @page)
+    @books = searcher.get_books
+    @message = searcher.get_message
 
     respond_to do |format|
       format.html { render 'search/index' }
@@ -64,60 +67,9 @@ class SearchController < ApplicationController
   end
 
   private
-    def get_apikey
-      return Apikey.first.key
-    end
 
-    def fetch(word, page)
-      params = URI.encode_www_form({applicationId: get_apikey, format: 'json', formatVersion: 2, keyword: word, hits: 20, page: page, sort: 'standard'})
-      uri = URI.parse("https://app.rakuten.co.jp/services/api/BooksTotal/Search/20170404?#{params}")
-      logger.debug('test logger')
-      # 新しくHTTPセッションを開始し、結果をresponseへ格納（Net::HTTPResponseのインスタンス）
-      response = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
-        http.open_timeout = 5
-        http.read_timeout = 10
-        http.get(uri.request_uri)
-      end
-      begin
-        case response
-        when Net::HTTPSuccess
-          @result = JSON.parse(response.body)
-          @books = Array.new
-          @result["Items"].each do |item|
-            @books << extract_item(item)
-          end
-        when Net::HTTPRedirection
-          @message = "Redirection: code=#{response.code} message=#{response.message}"
-        else
-          @message = "HTTP ERROR: code=#{response.code} message=#{response.message}"
-        end
-      rescue IOError => e
-        @message = "e.message"
-      rescue TimeoutError => e
-        @message = "e.message"
-      rescue JSON::ParserError => e
-        @message = "e.message"
-      rescue => e
-        @message = "e.message"
-      end
-    end
-
-    def extract_item(item)
-      return Book.new(
-        title: item["title"],
-        price: item["itemPrice"],
-        author: item["author"],
-        publisher: item["publisherName"],
-        isbn: item["isbn"],
-        caption: item["itemCaption"],
-        sales_date: item["salesDate"],
-        item_url: item["itemUrl"],
-        largeimage_url: item["largeImageUrl"]
-      )
-    end
-
-    def book_params
-      params.permit(:title, :price, :author, :publisher, :isbn, :caption, :sales_date, :item_url, :largeimage_url)
-    end
+  def book_params
+    params.permit(:title, :price, :author, :publisher, :isbn, :caption, :sales_date, :item_url, :largeimage_url)
+  end
 
 end
