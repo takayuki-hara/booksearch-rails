d4040966aac122110e84b5da642a5b79bd9fa458
takayuki-hara <takayuki.hara.dev@gmail.com>
2018-12-31 09:39:18 +0900

    管理画面の色を分ける対応、ナビゲーションのリファクタ

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def page_category
     return @page_category
   end
+
+  def bg_color
+    if page_category == 'manage'
+      return 'bg-danger'
+    else
+      return 'bg-info'
+    end
+  end
+
 end
