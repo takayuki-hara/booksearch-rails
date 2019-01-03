c6f61c74a795d4efc43e90a72d9655a57157bad9
takayuki-hara <takayuki.hara.dev@gmail.com>
2019-01-03 22:46:07 +0900

    お気に入り画面のリファクタ

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,5 @@
 class FavoritesController < ApplicationController
-  before_action :set_favorite, only: [:show, :edit, :update, :destroy]
+  before_action :set_favorite, only: [:destroy]
 
   # GET /favorites
   # GET /favorites.json
@@ -9,50 +9,6 @@ class FavoritesController < ApplicationController
       @favorites = Favorite.all
     else
       @favorites = Favorite.where(user_id: @current_user.id)
-    end
-  end
-
-  # GET /favorites/1
-  # GET /favorites/1.json
-  def show
-  end
-
-  # GET /favorites/new
-  def new
-    @favorite = Favorite.new
-  end
-
-  # GET /favorites/1/edit
-  def edit
-  end
-
-  # POST /favorites
-  # POST /favorites.json
-  def create
-    @favorite = Favorite.new(favorite_params)
-
-    respond_to do |format|
-      if @favorite.save
-        format.html { redirect_to @favorite, notice: 'Favorite was successfully created.' }
-        format.json { render :show, status: :created, location: @favorite }
-      else
-        format.html { render :new }
-        format.json { render json: @favorite.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /favorites/1
-  # PATCH/PUT /favorites/1.json
-  def update
-    respond_to do |format|
-      if @favorite.update(favorite_params)
-        format.html { redirect_to @favorite, notice: 'Favorite was successfully updated.' }
-        format.json { render :show, status: :ok, location: @favorite }
-      else
-        format.html { render :edit }
-        format.json { render json: @favorite.errors, status: :unprocessable_entity }
-      end
     end
   end
 
@@ -73,8 +29,4 @@ class FavoritesController < ApplicationController
     @favorite = Favorite.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def favorite_params
-    params.require(:favorite).permit(:user_id, :book_id)
-  end
 end
